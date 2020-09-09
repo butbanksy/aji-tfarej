daeb98c4b7f0688337427a0bb2a89306c3655bc2
Mahmoud Rhazzoul <56742148+butbanksy@users.noreply.github.com>
2020-09-09 05:28:48 +0100

    Added dark/light mode toggle. Started working on home page

---
+++ components/movies/SuggestedMovies.js
@@ -0,0 +1,7 @@
+import React from "react";
+import {Image, StyleSheet} from "react-native";
+import MovieCover from "./MovieCover";
+
+export default function SuggestedMovies(props) {
+    return <MovieCover/>;
+}
